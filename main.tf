bd0ca7a71b5d6b9004ff9c64de0fe387aabdd50a
pengchao <pengchao.ma2@outlook.com>
2025-09-21 12:06:06 +0800

    added the backend file

--- main.tf
+++ main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_version = ">= 1.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.region
-}
-
 module "ec2_instance" {
   source = "./modules/ec2"
 
