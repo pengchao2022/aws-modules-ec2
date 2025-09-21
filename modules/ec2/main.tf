52cdd810ad4b36fd7bda59a32453d19b74407c7f
pengchao <pengchao.ma2@outlook.com>
2025-09-21 13:21:12 +0800

    modified again

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -64,7 +64,7 @@ resource "tls_private_key" "ec2_ssh_key" {
 # 将私钥保存到本地文件
 resource "local_file" "private_key" {
   content  = tls_private_key.ec2_ssh_key.private_key_pem
-  filename = "${path.root}/ssh_key_file/${var.key_name}.pem"
+  filename = "${path.cwd}/ssh_key_file/${var.key_name}.pem"
 
   provisioner "local-exec" {
     command = "chmod 600 ${path.root}/ssh_key_file/${var.key_name}.pem"
